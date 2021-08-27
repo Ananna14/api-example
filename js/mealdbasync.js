c4cd7dc0a8efaf43817f72c6af2ea0eda9dde415
Ananna14 <anannasheikh89@gmail.com>
2021-08-27 08:28:16 -0700

    last-commit

--- js/mealdbasync.js
+++ js/mealdbasync.js
@@ -1,4 +1,4 @@
-const searchFood = () => {
+const searchFood = async () => {
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
     // console.log(searchText);
@@ -10,13 +10,17 @@ const searchFood = () => {
     }
     else {
         // load-data 
-        const url = `https://www.themealdb.com/api/json/v91/1/search.php?s=${searchText}`
+        const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`
         // console.log(url);
 
-        fetch(url)
-            .then(response => response.json())
-            .then(data => displaySearchResult(data.meals))
-            .catch(error => console.log(error));
+        const res = await fetch(url);
+        const data = await res.json();
+        displaySearchResult(data.meals)
+
+
+        // fetch(url)
+        //     .then(response => response.json())
+        //     .then(data => displaySearchResult(data.meals))
     }
 
 
@@ -48,17 +52,22 @@ const displaySearchResult = meals => {
 
 }
 
-const loadMealDetail = mealId => {
+const loadMealDetail = async mealId => {
     // console.log(mealId);
     const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId} `;
-    fetch(url)
-        .then(response => response.json())
-        .then(data => displayMealDetail(data.meals[0]))
+    const res = await fetch(url);
+    const data = await res.json();
+    displayMealDetail(data.meals[0])
+
+    // fetch(url)
+    //     .then(response => response.json())
+    //     .then(data => displayMealDetail(data.meals[0]))
 }
 
 const displayMealDetail = meal => {
     console.log(meal);
     const mealDetails = document.getElementById('meal-details');
+    mealDetails.textContent = ' ';
     const div = document.createElement('div');
     div.classList.add('card');
     div.innerHTML = `
